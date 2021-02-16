3f6af46ca0f2db56e323c75c5852f1ccec9110de
Martín Vergara <martinmorsevergara@gmail.com>
2021-02-15 23:35:51 -0300

    [FE] Fix en envío de parámetros por GET

--- vue-app/src/api/GivenGiftService.js
+++ vue-app/src/api/GivenGiftService.js
@@ -2,7 +2,7 @@ import client from '@/api/client'
 
 export default {
     getGivenGifts(birthID) {
-      return client.get('/given_gifts', { birth_id: birthID })
+      return client.get('/given_gifts', { params: { birth_id: birthID } })
     },
     postGivenGift(birthID, giftID) {
       return client.post('/given_gifts', { birth_id: birthID, gift_id: giftID })
--- vue-app/src/api/TimeslotService.js
+++ vue-app/src/api/TimeslotService.js
@@ -2,7 +2,10 @@ import client from '@/api/client'
 
 export default {
   get(birth_id) {
-    return client.get('/timeslots', {birth_id})
+    return client.get('/timeslots', { params: {birth_id} })
+  },
+  getAllAvailable(birth_id) {
+    return client.get('/timeslots', { params: {birth_id, available: true} })
   },
   createTimeslot(data) {
     return client.post('/timeslots', data)
