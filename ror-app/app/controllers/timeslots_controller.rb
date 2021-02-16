8e7c68d5fb9fa02d09da2237c4fdfda5103ab5b2
Martín Vergara <martinmorsevergara@gmail.com>
2021-02-16 03:34:45 -0300

    [BE] Agrega precondiciones en Timeslots#index. Para los que ya tienen una interacción  asignada, se devuelve sólo si cumple con los mecanismos del tipo de interacción (ej: que permita que se agregue más gente

--- ror-app/app/controllers/timeslots_controller.rb
+++ ror-app/app/controllers/timeslots_controller.rb
@@ -5,14 +5,45 @@ class TimeslotsController < ApplicationController
   def index
     if params[:birth_id]
       @timeslots = Timeslot.where(birth_id: params[:birth_id])
-      if params[:available]
-        @timeslots = @timeslots.where(interaction_id: nil)
-      end
     else
       @timeslots = Timeslot.all
     end
 
-    render json: @timeslots
+    if params[:available]
+      @available_timeslots = []
+
+
+      @timeslots.each { |timeslot|
+        timeslot_interaction = timeslot.interaction
+
+        if timeslot_interaction.nil?
+          @available_timeslots.append(timeslot)
+          next
+        else
+          timeslot_users_records = TimeslotUser.where(timeslot_id: timeslot.id).pluck(:user_id)
+          if timeslot_users_records.length() >= timeslot_interaction.allowed_attendees
+            next
+          end
+
+          if timeslot_users_records.include? current_user.id
+            next
+          end
+
+          interactions_of_that_day = Timeslot.where(interaction_id: timeslot_interaction.id)
+          interactions_of_that_day = interactions_of_that_day.where("DATE(timeslots.from) = ?", timeslot.from.to_date)
+          if interactions_of_that_day.length() >= timeslot_interaction.allowed_times_a_day
+            next
+          end
+        end
+
+        @available_timeslots.append(timeslot)
+      }
+
+      render json: @available_timeslots
+    else
+      render json: @timeslots
+    end
+
   end
 
   # GET /timeslots/1
@@ -33,7 +64,12 @@ class TimeslotsController < ApplicationController
 
   # PATCH/PUT /timeslots/1
   def update
-    if @timeslot.update(timeslot_params)
+    ActiveRecord::Base.transaction do
+      @timeslot_update = @timeslot.update!(timeslot_params)
+      TimeslotUser.create(timeslot_id: @timeslot.id, user_id: current_user.id)
+    end
+
+    if @timeslot_update
       render json: @timeslot
     else
       render json: @timeslot.errors, status: :unprocessable_entity
@@ -53,6 +89,6 @@ class TimeslotsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def timeslot_params
-      params.require(:timeslot).permit(:from, :to, :birth_id, :available)
+      params.require(:timeslot).permit(:from, :to, :birth_id, :available, :interaction_id)
     end
 end
